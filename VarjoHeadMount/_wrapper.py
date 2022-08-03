9746d92a2441b3576c31f3d593673dbb945c0ac0
loranjenkins <loranbogaart@outlook.com>
2022-08-03 18:45:06 +0200

    update

--- VarjoHeadMount/_wrapper.py
+++ VarjoHeadMount/_wrapper.py
@@ -2,6 +2,7 @@ import os
 import time
 import ctypes
 import sys
+from datetime import datetime
 
 #
 # from VarjoHeadMount._state import Session, SessionInit
@@ -27,13 +28,31 @@ import sys
 #     """
 #
 #     return _dll_handle.varjo_FrameGetPose(ctypes.c_int(posetype))
-import numpy as np
+
 
 
 class Matrix4x4(ctypes.Structure):
     _fields_ = [
         ('value', ctypes.c_double * 16),
     ]
+
+class ViewInfo(ctypes.Structure):
+    _fields_ = [
+        ('enabled', ctypes.c_int32),
+        ('preferredHeight', ctypes.c_int32),
+        ('preferredWidth', ctypes.c_int32),
+        ('projectionMatrix', ctypes.c_double * 16),
+        ('reserved', ctypes.c_int32),
+        ('viewMatrix', ctypes.c_double * 16),
+    ]
+
+class Frameinfo(ctypes.Structure):
+    _fields_ = [
+        ('displayTime', ctypes.c_int64),
+        ('frameNumber', ctypes.c_int64),
+        ('views', ctypes.POINTER(ViewInfo)),
+    ]
+
 
 if __name__ == '__main__':
     # import dll and define return types for all functions
@@ -48,6 +67,9 @@ if __name__ == '__main__':
     _dll_handle.varjo_GetError.restype = ctypes.c_int64
     _dll_handle.varjo_GetViewCount.restype = ctypes.c_int32
     _dll_handle.varjo_SessionInit.restype = ctypes.POINTER(ctypes.c_void_p)
+    _dll_handle.varjo_FrameGetDisplayTime.restype = ctypes.c_int64
+    _dll_handle.varjo_GetCurrentTime.restype = ctypes.c_int64
+    _dll_handle.varjo_CreateFrameInfo.restype = Frameinfo
 
 
     varjo_session_pointer = _dll_handle.varjo_SessionInit()
@@ -64,18 +86,28 @@ if __name__ == '__main__':
     print('-------------------------')
     print(_dll_handle.varjo_GetViewCount(varjo_session_pointer))
 
+    info = _dll_handle.varjo_CreateFrameInfo(varjo_session_pointer)
 
     #
     _dll_handle.varjo_CreateFrameInfo.restype = ctypes.POINTER(ctypes.c_void_p)
     varjo_frameinfo_pointer = _dll_handle.varjo_CreateFrameInfo(varjo_session_pointer)
     _dll_handle.varjo_WaitSync(varjo_session_pointer, varjo_frameinfo_pointer)
 
+    # _dll_handle.varjo_FrameGetDisplayTime(varjo_session_pointer)
 
-    for i in range(100):
-        _dll_handle.varjo_WaitSync(varjo_session_pointer, varjo_frameinfo_pointer)
-        matrix = _dll_handle.varjo_FrameGetPose(varjo_session_pointer, ctypes.c_int64(2))
-        print(list(matrix.value))
-        time.sleep(5)
+    # for i in range(100):
+    # while True:
+    #
+    #     _dll_handle.varjo_WaitSync(varjo_session_pointer, varjo_frameinfo_pointer)
+    #     matrix = _dll_handle.varjo_FrameGetPose(varjo_session_pointer, ctypes.c_int64(2))
+    #     matrix = list(matrix.value)
+    #
+    #     epoch = _dll_handle.varjo_FrameGetDisplayTime(varjo_session_pointer)
+    #     time = _dll_handle.varjo_GetCurrentTime(varjo_session_pointer)
+    #     dt = datetime.fromtimestamp(time // 1000000000)
+    #     print('Time since epoch in nanosecond:', dt, 'Pose with 1 straight -1 backwards:', matrix[10])
+
+        # time.sleep(0.5)
 
 
 
